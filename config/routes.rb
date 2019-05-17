787e7a02b7b753a1651beacdecaffb18d3e3e789
mitsufumi.katanoda <mkata0711@gmail.com>
2019-05-17 05:44:25 +0000

    CRUD tasks

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,22 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # CRUD
+  # get 'tasks/:id', to: 'task#show'
+  # post 'tasks', to: 'tasks#create'
+  # put 'tasks/:id', to: 'tasks#update'
+  # delete 'tasks/:id', to: 'tasks#destroy'
+  
+  # index: showの補助ページ
+  # get 'tasks', to: 'tasks#index'
+  
+  # new: 新規作成用のフォームページ
+  # get 'tasks/new', to: 'tasks#new'
+  
+  # edit: 更新用のフォームページ
+  # get 'tasks/:id/edit', to: 'tasks#edit'
+  
+  # タスクの Router には resources を利用してください。
+  root to: 'tasks#index'
+  
+  resources :tasks
 end
